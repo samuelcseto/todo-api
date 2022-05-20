b6bdfaf65bb42edbff3af55a79e9961bebb2a2a1
scseto <samuelcseto@gmail.com>
2022-05-20 23:09:32 +0200

    fix: find users not loggedin

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -7,7 +7,7 @@ const {
 module.exports = {
   before: {
     all: [],
-    find: [  ],
+    find: [ authenticate('jwt') ],
     get: [ authenticate('jwt') ],
     create: [ hashPassword('password') ],
     update: [ hashPassword('password'),  authenticate('jwt') ],
